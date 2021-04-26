b83bb737495da3013ca641a908d49600e406b131
chrisweeting <chrisweeting@gmail.com>
2021-04-26 13:23:20 -0400

    DOMNodeCollection append

--- src/dom_node_collection.js
+++ src/dom_node_collection.js
@@ -4,14 +4,35 @@ class DOMNodeCollection {
     }
 
     html(string) {
-        
+        if (typeof string === "string") {
+            this.each( (node) => {
+                node.innerHTML = string;
+            });
+        } else if (this.htmlEleArr.length > 0) {
+            return this.htmlEleArr[0].innerHTML;
+        }
     }
 
     empty() {
-
+        this.html('');
     }
 
-    append() {
+    append(child) {
+        if (this.htmlEleArr.length === 0) {
+            return;
+        } 
+
+        if (typeof child === "string") {
+            this.each((node) => {
+                node.innerHTML += child;
+            });
+        } else if (child instanceof DOMNodeCollection) {
+            this.each ((node) => {
+                child.each ((childNode) => {
+                    node.appendChild(childNode);
+                });
+            });
+        }
 
     }
 
